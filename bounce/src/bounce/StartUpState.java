aca404d1415b670658b408e3b6a234330b5e966f
Jasmine Emerson <jazafrazzle@gmail.com>
2016-09-13 23:26:58 -0700

    Customized splash screen to include name and instructions. Changed square to a ball.

--- bounce/src/bounce/StartUpState.java
+++ bounce/src/bounce/StartUpState.java
@@ -45,7 +45,7 @@ class StartUpState extends BasicGameState {
 		for (Bang b : bg.explosions)
 			b.render(g);
 		g.drawImage(ResourceManager.getImage(BounceGame.STARTUP_BANNER_RSC),
-				225, 270);		
+				225, 120);		
 	}
 
 	@Override
